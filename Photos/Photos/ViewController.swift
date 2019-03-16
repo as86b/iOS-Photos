27e4338b69fee961c3bb5cd143e374ba672921d5
Austin Sizemore <austinsizemore@austins-mbp.lan>
2019-03-15 23:55:46 -0500

    Added team error and a few other code things

--- Photos/Photos/ViewController.swift
+++ Photos/Photos/ViewController.swift
@@ -28,15 +28,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func openCamera(_ sender: Any) {
-        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        }
+        else {
             print("Camera not supported by this device")
             return
         }
-            
-        imagePicker.sourceType = .camera
-        imagePicker.delegate = self
-        
-        present(imagePicker, animated: true)
     }
     
     
@@ -52,4 +52,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // get the image
+        guard let image = info[UIImagePickerControllerOriginalImage.] as? UIImage else {
+            return
+        }
+        
+        // do something with it
+        imageView.image = image
+    }
 }
