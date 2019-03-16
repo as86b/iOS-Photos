e1fa13e94f089e692f9a8fab1cb78169d995250a
Austin Sizemore <austinsizemore@austins-mbp.lan>
2019-03-15 22:54:34 -0500

    Added in code

--- Photos/Photos/ViewController.swift
+++ Photos/Photos/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//https://www.weheartswift.com/get-images-camera-photo-library/
+//Was pretty helpful
+
+//Note that in info.plist, I added in the privacy opiton to gain access
+//Description in link above of how to do so
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
     }
-
-
+    
+    let imagePicker = UIImagePickerController()
+    
+    //Difference between weak and strong?
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func openCamera(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("Camera not supported by this device")
+            return
+        }
+            
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true)
+    }
+    
+    
+    @IBAction func openImagesFolder(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("Can't open photo library")
+            return
+        }
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true)
+    }
+    
 }
-
